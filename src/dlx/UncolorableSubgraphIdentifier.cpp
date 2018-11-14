f335bcac87776b56a1ca9a2fd1582303cab5dda7
Willy Tai <b04502139@ntu.edu.tw>
2018-11-14 22:19:53 +0800

    propagate meeting version

--- src/dlx/UncolorableSubgraphIdentifier.cpp
+++ src/dlx/UncolorableSubgraphIdentifier.cpp
@@ -23,32 +23,52 @@ void UncolorableSubgraphIdentifier::run() {
 }
 
 bool UncolorableSubgraphIdentifier::propagate(Vertex* currentVertex) {
-    if (currentVertex->isGlobalRef()) return false;      // a dead end occurs
+    // if (currentVertex->isGlobalRef()) return false;      // a dead end occurs
+    // if no other vertexes are left to try, and currentVertex is colorable, then return true
+    bool* dummy = new bool(false);
+    vector<bool*> color_checker; color_checker.resize(5, dummy); // index 0 is dummy node, index 4 is for UNDEF, only 1~3 is actually used
+    bool determined = true;
+    bool colorable  = false;
+    for (auto it = currentVertex->VertexList.begin(); it != currentVertex->VertexList.end(); ++it) {
+        if (!_vertex[(*it)->ID]) continue;
+        if (!(*it)->isGlobalRef()) { determined = false; } // there are still some other vertexes left
+        else color_checker[int((*it)->color)] = NULL;
+    }
+    for (size_t i = 1; i <= 3; ++i) {
+        if (color_checker[i] && determined) { cout << "vertex " << *currentVertex << " is colorable" << endl; return true; } // colorable
+        if (color_checker[i]) colorable = true;
+    }
+    if (!colorable) return false;                        // if found vertex uncolorable, just return
 
     const Color originalColor = currentVertex->color;    // original color, need to restore it back to allow further traversal
     currentVertex->setToGlobalRef();                     // mark the visited vertex
 
-    bool returnValue = true;
+    bool returnValue = true;                             // this will be set to false if any propagate called bellow returns false
     for (int i = 1; i <= 3; ++i) {
         if (COLORS[i] == originalColor) continue;        // avoid its parent's color
 
-        cout << "currentVertex: " << *currentVertex << ", trying color " << COLORS[i] << "; ";
 
         currentVertex->color = COLORS[i];
         for (auto it = currentVertex->VertexList.begin(); it != currentVertex->VertexList.end(); ++it) {
             if ((*it)->color != currentVertex->color) continue;     // skip the non-conflict vertexes
+            if ((*it)->isGlobalRef()) break;                        // if a vertex is already visited and forms a conflict, then this color is not available
+                                                                    // needs to try a different one
 
+            cout << "currentVertex: " << *currentVertex << ", trying color " << COLORS[i] << "; ";
             cout << "found conflict with vertex: " << *(*it) << ", propagate" << endl;
-            
+
             if (!propagate(*it)) {                       // mark edge (currentVertex, *it) as a conflict edge
                 cout << "(" << *currentVertex << ", " << *(*it) << ") is a conflict edge" << endl;
                 returnValue = false;                     // returns false if at least the value of one branch is false
             }
+            // else returnValue |= true;
         }
     }
     currentVertex->color = originalColor;                // restore the color
+    currentVertex->unSetGlobalRef();                     // unmark the vertex
     cout << "currentVertex: " << *currentVertex << ", coloring it back to " << originalColor << endl;
     cout << "returning " << returnValue << " at vertex: " << *currentVertex << endl;
+    delete dummy;  // free up memory
     return returnValue;
 }
 
